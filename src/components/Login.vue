9224b86efc133500cf4ef58b8bf20f26d905e045
zhangyuwei <zhangyuwei@kingsware.cn>
2021-07-09 21:45:56 +0800

    second commit

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,16 +1,14 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <div>Login</div>
 </template>
 
 <script>
 export default {
-  name: 'App',
+  name: 'Login',
   data() {
     return {};
   },
 };
 </script>
 
-<style lang="less"></style>
+<style lang="less" scoped></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,28 +1,23 @@
-import Vue from "vue";
-import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
 Vue.use(VueRouter);
 
 const routes = [
   {
-    path: "/",
-    name: "Home",
-    component: Home,
+    path: '/',
+    redirect: '/login',
   },
   {
-    path: "/about",
-    name: "About",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
+    path: '/login',
+    name: 'Login',
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
+      import(/* webpackChunkName: "about" */ '../components/Login.vue'),
   },
 ];
 
 const router = new VueRouter({
-  mode: "history",
+  mode: 'history',
   base: process.env.BASE_URL,
   routes,
 });
